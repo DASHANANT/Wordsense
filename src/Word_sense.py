fc207193b9e1c7b402b0e4b724997416668acb81
Anant Dashpute <70088125+DASHANANT@users.noreply.github.com>
2021-10-31 11:43:52 +0530

    Add files via upload

--- src/Word_sense.py
+++ src/Word_sense.py
@@ -1,31 +1,64 @@
-from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
+import nltk
+nltk.download('stopwords')
+stopwords = set(stopwords.words('english'))
+
+functionwords = {'everyone', 'himself', 'it', 'his', 'everything', 'little', 'those', 'inside', 'on', 'off', 'over',
+                 'of', 'first', 'within', 'around', 'near', 'so', 'would', 'else', 'for', 'moreover', 'besides',
+                 'into', 'while', 'here', 'never', 'such', 'each', 'who', 'anyone', 'through', 'despite', 'might',
+                 'that', 'will', 'anything', 'in', 'therefore', 'your', 'someone', 'a', 'few', 'do', 'second', 'down',
+                 'themself', 'usually', 'one', 'with', 'any', 'onto', 'all', 'to', 'must', 'herself', 'him', 'most', 'much',
+                 'but', 'along', 'should', 'my', 'an', 'no', 'against', 'before', 'could', 'now', 'there', 'meanwhile',
+                 'be', 'instead', 'during', 'them', 'from', 'less', 'if', 'something', 'ones', 'he', 'two', 'sometimes',
+                 'yours', 'have', 'however', 'otherwise', 'its', 'though', 'often', 'toward', 'than', 'their', 'then',
+                 'half', 'least', 'although', 'nothing', 'her', 'next', 'as', 'across', 'always', 'many', 'how', 'anyway',
+                 'when', 'this', 'behind', 'own', 'both', 'at', 'itself', 'last', 'hers', 'other', 'they', 'our',
+                 'incidentally', 'may', 'whose', 'beside', 'without', 'about', 'she', 'some', 'where', 'can', 'and',
+                 'because', 'every', 'theirs', 'twice', 'another', 'since', 'what', 'after', 'which', 'these', 'more',
+                 'shall', 'by', 'several', 'the', 'or'}
+
+stopwords = stopwords.update(functionwords)
 ps = PorterStemmer()
 lm = WordNetLemmatizer()
 
-                 
+
 def stem(word):
     word = lm.lemmatize(word)
     if wordnet.synsets(ps.stem(word)) == wordnet.synsets(word):
         return ps.stem(word)
     else:
         return word
-        
+
+
 def Get_AmbiguousWords(sentence):
     context = set(word_tokenize(sentence))
     context = list(context)
     for i in range(len(context)):
-        context[i]=str(stem(context[i])) 
-    context = set(context).difference(stopwords)    
-    AmbiguousWords=[]
-    for i in context:            
-        if len(wordnet.synsets(i))>2:
+        context[i] = str(stem(context[i]))
+    context = set(context).difference(stopwords)
+    AmbiguousWords = []
+    for i in context:
+        if len(wordnet.synsets(i)) > 2:
             AmbiguousWords.append(i)
     return AmbiguousWords
-    
-    
-def Get_Sense(sentence, ambiguous_word, pos=None, synsets=None):       
+
+
+def Get_Sense(sentence, ambiguous_word, pos=None, synsets=None):
+
+    def tokenized_sent(self, k):
+        string = str(k)
+        tokens = set(word_tokenize(string))
+        tokens = list(tokens)
+        for i in range(len(tokens)):
+            tokens[i] = str(self.stem(tokens[i]))
+        tokens = set(tokens)
+        return tokens
+
     context = set(sentence)
     context = [stem(w) for w in context]
 
@@ -54,7 +87,7 @@ def Get_Sense(sentence, ambiguous_word, pos=None, synsets=None):
                 re.sub('[^a-zA-Z.\d\s]', '', str(s.examples()))))
 
         # "to remove stopwords"
-        gloss = gloss.difference(stopword)
+        gloss = gloss.difference(stopwords)
         gloss = list(gloss)
         for i in range(len(gloss)):
             # "Stemming each word"
@@ -78,8 +111,5 @@ def Get_Sense(sentence, ambiguous_word, pos=None, synsets=None):
             max_overlap = overlap
             best_sense = synsets[k]
 
-    return best_sense    
+    return best_sense
     #"Returns best sense of the word"
-
-
-
